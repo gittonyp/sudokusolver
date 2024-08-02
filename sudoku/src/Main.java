552408cac735d1b10eb3580762b5fb8ebc9e8e49
Tony Ganesh Prajapati <133465084+gittonyp@users.noreply.github.com>
2024-08-02 13:42:30 +0530

    Add files via upload

---
+++ sudoku/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+       public static void main(String[] args) {
+        int board[][]={{ 3, 1, 6, 5, 7, 8, 4, 9, 2 },
+            { 5, 2, 9, 1, 3, 4, 7, 6, 8 },
+            { 4, 8, 7, 6, 2, 9, 5, 3, 1 },
+            { 2, 6, 3, 0, 1, 5, 9, 8, 7 },
+            { 9, 7, 4, 8, 6, 0, 1, 2, 5 },
+            { 8, 5, 1, 7, 9, 2, 6, 4, 3 },
+            { 1, 3, 8, 0, 4, 7, 2, 0, 6 },
+            { 6, 9, 2, 3, 5, 1, 8, 7, 4 },
+            { 7, 4, 5, 0, 8, 6, 3, 1, 0 } };
+
+      sudokusolver sud=new sudokusolver();
+      sud.sudokua(board,0,0);
+
+    }
+}
+class sudokusolver{
+    int pi=-1,pj=-1;
+    boolean sudokua(int board[][],int i,int j){
+
+    return true;
+    }
+
+    int[] check(int board[][],int i,int j){
+        for (int k = 0; k < 9; k++) {
+
+        }
+        for (int k = 0; k < 9; k++) {
+
+        }
+        for (int k = 0; k < ; k++) {
+            for (int l = 0; l < ; l++) {
+
+            }
+        }
+    }
+
+
+}
